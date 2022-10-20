4e49c3becf390969aedbcd3352a5864a29e59ce2
monicahwamuhu <monicahwamuhu2@gmail.com>
2022-10-20 13:24:47 -0700

    team project

---
+++ printf_37.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *
+ *  * printf_37 - prints the char 37.
+ *
+ *   * Return: 1.
+ *
+ *    */
+
+int printf_37(void)
+
+{
+
+		_putchar(37);
+
+			return (1);
+
+}
+
